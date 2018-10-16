09d2de0ff53aad3638e7e72ecc91f506d3a6fc43
Chris <chris.ruegger@gmail.com>
2018-10-15 20:02:33 -0400

    Integration Test

--- ProductService/src/test/java/com/rueggerllc/productservice/test/integration/CommonSteps.java
+++ ProductService/src/test/java/com/rueggerllc/productservice/test/integration/CommonSteps.java
@@ -1,6 +1,5 @@
 package com.rueggerllc.productservice.test.integration;
 
-import cucumber.api.PendingException;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
@@ -9,13 +8,18 @@ public class CommonSteps extends SpringBootBaseIntegrationTest {
 	@When("User Wants to Retrieve All Products")
 	public void user_Wants_to_Retrieve_All_Products() {
 	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
+	    // throw new PendingException();
+		System.out.println("User Wants to Retrieve All Products");
 	}
 
 	@Then("All Products are Retrieved for User")
 	public void all_Products_are_Retrieved_for_User() {
 	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
+	    // throw new PendingException();
+		
+		System.out.println("All Producs are Retrieved for User");
+		serviceTester.doIt();
+		serviceTester.getAllProducts(testRestTemplate);
 	}
 	
 }
